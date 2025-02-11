475b5aae9e9cf91681edd316b0ae6b312a58a581
arianna choza <unachoza@gmail.com>
2025-02-11 18:23:25 -0500

    using radio svg for checked

--- src/components/Form/RadioInput.tsx
+++ src/components/Form/RadioInput.tsx
@@ -1,5 +1,6 @@
 import { FormEventHandler, useState } from "react";
 import "./Form.css";
+import RadioSelected from "../../assets/images/icon-radio-selected.svg";
 
 interface RadioInputProps {
 	id: number;
@@ -16,18 +17,13 @@ const RadioInput = (props: RadioInputProps) => {
 	const { name, value, label } = props;
 	return (
 		<div className="radio-input-container">
-			<input
-				type="radio"
-				id={name}
-				name={name}
-				value={value}
-				checked
-			/>
-			<span className="radio-button"></span>
+			<input type="radio" id={name} name={name} value={value} checked />
+			<span className="radio-button">
+				<img src={RadioSelected} alt="" />
+			</span>
 			<label htmlFor={name}>{label}</label>
 		</div>
 	);
 };
 
 export default RadioInput;
-
